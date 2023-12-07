cae5297b7e4c4fc25c9df30ebf67ddaf0895ebcc
wwj718 <wuwenjie718@gmail.com>
2023-12-07 22:52:51 +0800

    add demo

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <BleKeyboard.h>
+
+BleKeyboard bleKeyboard;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+  bleKeyboard.begin();
+}
+
+void loop() {
+  if(bleKeyboard.isConnected()) {
+    Serial.println("Sending 'Hello world'...");
+    bleKeyboard.print("Hello world");
+  }
+  Serial.println("Waiting 5 seconds...");
+  delay(3000);
+}
